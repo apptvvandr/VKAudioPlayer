d97f4b47297c51eb4edfa5fa7ae656f9a11396c3
y0rrrsh <artur.ersh@gmail.com>
2016-05-20 19:29:03 +0300

    Fixed crash caused by selector

--- VkAudio/AppDelegate.swift
+++ VkAudio/AppDelegate.swift
@@ -9,6 +9,7 @@
 import UIKit
 import CoreData
 import AVFoundation
+import MediaPlayer
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
         try! AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+        
+        let commandCenter = MPRemoteCommandCenter.sharedCommandCenter()
+        commandCenter.previousTrackCommand.addTargetWithHandler { (event: MPRemoteCommandEvent) -> MPRemoteCommandHandlerStatus in
+            AudioPlayer.sharedInstance.playPrevious()
+            return .Success
+        }
+        commandCenter.nextTrackCommand.addTargetWithHandler { (event: MPRemoteCommandEvent) -> MPRemoteCommandHandlerStatus in
+            AudioPlayer.sharedInstance.playNext()
+            return .Success
+        }
+        commandCenter.playCommand.addTargetWithHandler { (event: MPRemoteCommandEvent) -> MPRemoteCommandHandlerStatus in
+            AudioPlayer.sharedInstance.continuePlaying()
+            return .Success
+        }
+        commandCenter.pauseCommand.addTargetWithHandler { (event: MPRemoteCommandEvent) -> MPRemoteCommandHandlerStatus in
+            AudioPlayer.sharedInstance.pause()
+            return .Success
+        }
+        
         return true
     }
     
