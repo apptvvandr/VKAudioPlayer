bcfd5fda47bc71ace1a1bd41202bae0243d88bb1
y0rrrsh <artur.ersh@gmail.com>
2016-05-30 20:14:58 +0300

    Implemented audio player control view

---
+++ Android/VKAudioPlayer/app/src/main/java/github/y0rrrsh/vkaudioplayer/views/PlaybackControlView.java
@@ -0,0 +1,136 @@
+package github.y0rrrsh.vkaudioplayer.views;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
+
+import github.y0rrrsh.vkaudioplayer.R;
+
+/**
+ * @author Artur Yorsh. 30.05.16.
+ */
+public class PlaybackControlView extends LinearLayout {
+
+    private ImageButton btnPrevious;
+    private ImageButton btnPlay;
+    private ImageButton btnNext;
+    private ImageButton btnShuffle;
+    private ImageButton btnRepeat;
+    private SeekBar seekProgress;
+
+    private ActionHandler actionHandler;
+
+    public PlaybackControlView(Context context) {
+        super(context);
+        init();
+    }
+
+    public PlaybackControlView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public PlaybackControlView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public PlaybackControlView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init();
+    }
+
+    private void init() {
+        View v = LayoutInflater.from(this.getContext()).inflate(R.layout.view_player_control, this, true);
+
+        btnPrevious = (ImageButton) v.findViewById(R.id.btn_player_previous);
+        btnPlay = (ImageButton) v.findViewById(R.id.btn_player_play);
+        btnNext = (ImageButton) v.findViewById(R.id.btn_player_next);
+        btnShuffle = (ImageButton) v.findViewById(R.id.btn_player_shuffle);
+        btnRepeat = (ImageButton) v.findViewById(R.id.btn_player_repeat);
+        seekProgress = (SeekBar) v.findViewById(R.id.seek_player_progress);
+
+        btnPrevious.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v1) {
+                if (actionHandler == null) return;
+                actionHandler.onPreviousClicked();
+
+            }
+        });
+        btnPlay.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v1) {
+                if (actionHandler == null) return;
+                actionHandler.onPlayClicked();
+            }
+        });
+        btnNext.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v1) {
+                if (actionHandler == null) return;
+                actionHandler.onNextClicked();
+            }
+        });
+        btnShuffle.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v1) {
+                if (actionHandler == null) return;
+                actionHandler.onShuffleClicked();
+            }
+        });
+        btnRepeat.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v1) {
+                if (actionHandler == null) return;
+                actionHandler.onRepeatClicked();
+            }
+        });
+        seekProgress.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                if (actionHandler == null) return;
+
+                int currentValue = seekBar.getProgress();
+                actionHandler.onSeekDragged(currentValue);
+            }
+        });
+    }
+
+    public void setActionHandler(ActionHandler actionHandler) {
+        this.actionHandler = actionHandler;
+    }
+
+    public interface ActionHandler {
+
+        void onPreviousClicked();
+
+        void onPlayClicked();
+
+        void onNextClicked();
+
+        void onShuffleClicked();
+
+        void onRepeatClicked();
+
+        void onSeekDragged(int currentValue);
+
+    }
+}
