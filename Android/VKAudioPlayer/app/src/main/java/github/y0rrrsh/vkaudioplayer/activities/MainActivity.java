dc4c579cd196a3b400abe71a094677c7b67b2226
y0rrrsh <artur.ersh@gmail.com>
2016-06-09 12:36:32 +0300

    Implemented navigation drawer

--- Android/VKAudioPlayer/app/src/main/java/github/y0rrrsh/vkaudioplayer/activities/MainActivity.java
+++ Android/VKAudioPlayer/app/src/main/java/github/y0rrrsh/vkaudioplayer/activities/MainActivity.java
@@ -4,17 +4,24 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
-import android.util.Log;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.squareup.picasso.MemoryPolicy;
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.RequestCreator;
 import com.squareup.picasso.Transformation;
 
 import butterknife.BindView;
 import github.y0rrrsh.vkapi.VKApi;
+import github.y0rrrsh.vkapi.VKApi.VKCallback;
 import github.y0rrrsh.vkaudioplayer.R;
 import github.y0rrrsh.vkaudioplayer.activities.common.BaseActivity;
 import github.y0rrrsh.vkaudioplayer.adapters.VkTabAdapter;
@@ -27,11 +34,12 @@ import jp.wasabeef.picasso.transformations.BlurTransformation;
  */
 public class MainActivity extends BaseActivity {
 
-    private static final String TAG = "MainActivity";
-
+    @BindView(R.id.toolbar) Toolbar toolbar;
     @BindView(R.id.tab_layout) TabLayout tabLayout;
     @BindView(R.id.view_pager) ViewPager viewPager;
     @BindView(R.id.image_tabs_bg) ImageView imageBackground;
+    @BindView(R.id.drawer_layout) DrawerLayout drawerLayout;
+    @BindView(R.id.navigation_view) NavigationView navigationView;
 
     @Override
     protected int getLayoutId() {
@@ -42,25 +50,56 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.desc_drawer_open, R.string.desc_drawer_close);
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.menu_nav_downloads:
+                    // TODO: 09.06.16 present downloads
+                    break;
+                case R.id.menu_nav_settings:
+                    // TODO: 09.06.16 present settings
+                    break;
+                case R.id.menu_nav_about:
+                    // TODO: 09.06.16 present app info
+                    break;
+                case R.id.menu_nav_logout:
+                    // TODO: 09.06.16 vk logout
+                    break;
+            }
+            drawerLayout.closeDrawers();
+            return false;
+        });
+
         viewPager.setAdapter(new VkTabAdapter(getSupportFragmentManager()));
         tabLayout.setupWithViewPager(viewPager);
 
         //noinspection ConstantConditions
-        VKAPServiceImpl.getInstance().getUserInfo(VKApi.USER_ID, new VKApi.VKCallback<UserDTO>() {
+        VKAPServiceImpl.getInstance().getUserInfo(VKApi.USER_ID, new VKCallback<UserDTO>() {
             @Override
             public void onResponse(UserDTO user) {
                 int blurRadius = MainActivity.this.getResources().getInteger(R.integer.blur_radius);
                 Transformation blur = new BlurTransformation(MainActivity.this, blurRadius);
-                Picasso.with(MainActivity.this)
+                RequestCreator picassoRequest = Picasso.with(MainActivity.this)
                         .load(user.getPhotoBig())
                         .memoryPolicy(MemoryPolicy.NO_CACHE, MemoryPolicy.NO_STORE)
-                        .transform(blur)
-                        .into(imageBackground);
+                        .transform(blur);
+
+                View headerView = navigationView.getHeaderView(0);
+                ImageView imageAvatar = (ImageView) headerView.findViewById(R.id.drawer_image_user_avatar);
+                TextView textUserName = (TextView) headerView.findViewById(R.id.drawer_text_user_name);
+
+                picassoRequest.into(imageAvatar);
+                picassoRequest.into(imageBackground);
+
+                textUserName.setText(user.getFirstName() + " " + user.getLastName());
             }
 
             @Override
             public void onError(Throwable t) {
-                Log.d(TAG, "onError: ");
             }
         });
     }
